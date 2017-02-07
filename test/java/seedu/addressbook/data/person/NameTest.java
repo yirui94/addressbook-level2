edba5873c03be5d7460b71fc14df329922228c1b
Yi Rui Goh <YiRui@184-110.priv23.nus.edu.sg>
2017-02-07 16:01:30 +0800

    Initialise NameTest

---
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -0,0 +1,14 @@
+package seedu.addressbook.data.person;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NameTest {
+
+    @Test
+    public void isNameSimilar() {
+        fail("Not yet implemented");
+    }
+
+}
